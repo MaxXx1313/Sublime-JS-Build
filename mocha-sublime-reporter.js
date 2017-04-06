0b3d58cea04323e700af9b5cfbd069ae087319da
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-04-06 16:07:51 +0300

    first version

---
+++ mocha-sublime-reporter.js
@@ -0,0 +1,73 @@
+// var mocha = require('mocha');
+module.exports = MyReporter;
+
+function MyReporter(runner) {
+  // mocha.reporters.Base.call(this, runner);
+  var passes = 0;
+  var failures = 0;
+
+  // var MAX_MESSAGE_LENGTH = 2048;
+
+  runner.on('pass', function(test){
+    passes++;
+    // TODO: map source
+    console.log('pass: [%s]', test.fullTitle());
+  });
+  //  "file_regex":"\\s*at\\s*(.*?):(\\d+):(\\d+)()"
+  runner.on('fail', function(test, err){
+    failures++;
+
+    var errMessage = err.message.replace(/\n/g, ' ');
+    // if(errMessage.length > MAX_MESSAGE_LENGTH){
+    //   errMessage = errMessage.substr(0, MAX_MESSAGE_LENGTH)+'...';
+    // }
+
+    ({file, line, col} = getErrorSource(err));
+    console.log('fail: [%s] at %s:%s:%s', test.fullTitle(), file, line, col,  errMessage);
+  });
+
+  runner.on('end', function(){
+    console.log('Pass %d of %d', passes, passes + failures);
+    if(failures > 0){
+      console.log('FAILED');
+    } else {
+      console.log('SUCCEED');
+    }
+    process.exit(failures);
+  });
+}
+
+MyReporter.getErrorSource = getErrorSource;
+
+
+/**
+ * TODO: SyntaxError?
+ */
+function getErrorSource(err){
+  // console.log('getErrorSource', err.stack);
+  var errString = err.stack.toString();
+
+  let fileInfo;
+  // TODO: compare by type?
+  if(errString.indexOf('AssertionError:') >= 0 ){
+    // AssertionError
+    errString = errString.replace(/\n/g, ' ').trim();
+    fileInfo = errString.match(/.*\s*at (.*?):(\d+):(\d+)$/) || [];
+    // console.log(fileInfo);
+  } else {
+    // common error
+    errString = errString.split('\n', 2).join(' ').trim();
+    // console.log('errString', errString)
+    fileInfo = errString.match(/\s*at\s*.*?\((.*?):(\d+):(\d+)\)$/) || [];
+  }
+  // console.log(stack);
+  var file = fileInfo[1];
+  var line = fileInfo[2];
+  var col  = fileInfo[3];
+
+  return {
+    file:file,
+    line:line,
+    col:col
+  };
+}
---
+++ mocha-sublime-reporter.spec.js
@@ -0,0 +1,133 @@
+
+
+// todo: move to a separate file
+const assert = require('assert');
+const AssertionError = require('assert').AssertionError;
+const MyReporter = require('./mocha-sublime-reporter.js');
+
+describe('getErrorSource [UNIT]', function(){
+
+
+  it('Generic Error #1', function(){
+    let input = 'Error: error 1\n    at timeout.then (throttle.js:26:15)'
+    let expected = {
+      file:'throttle.js',
+      line:'26',
+      col:'15'
+    };
+
+    let actual = MyReporter.getErrorSource({stack:input});
+
+    assert.deepEqual(actual, expected);
+
+  });
+
+
+  it('Generic Error #2', function(){
+    let input = 'TypeError: throttle(...).catch is not a function\n\
+        at Context.<anonymous> (D:\\sandbox\\throttle.js:74:43)\n\
+        at callFn (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runnable.js:343:21)\n\
+        at Test.Runnable.run (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runnable.js:335:7)\n\
+        at Runner.runTest (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:444:10)\n\
+        at C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:550:12\n\
+        at next (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:361:14)\n\
+        at C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:371:7\n\
+        at next (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:295:14)\n\
+        at C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:334:7\n\
+        at done (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runnable.js:293:5)\n\
+        at callFn (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runnable.js:361:7)\n\
+        at Hook.Runnable.run (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runnable.js:335:7)\n\
+        at next (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:309:10)\n\
+        at Immediate.<anonymous> (C:\\Users\\guest.user\\AppData\\Roaming\\npm\\node_modules\\mocha\\lib\\runner.js:339:5)\n\
+        at runCallback (timers.js:574:20)\n\
+        at tryOnImmediate (timers.js:554:5)\n\
+        at processImmediate [as _immediateCallback] (timers.js:533:5)';
+
+    let expected = {
+      file:'D:\\sandbox\\throttle.js',
+      line:'74',
+      col:'43'
+    };
+
+    let actual = MyReporter.getErrorSource({stack:input});
+
+    assert.deepEqual(actual, expected);
+
+  });
+
+
+  it('AssertionError #1', function(){
+    let input = "AssertionError: [ 'start 1',\n  'stop 5' ] deepEqual [ 'start 1',\n  'stop 5' ]\n    at throttle.js:66:14";
+    let expected = {
+        file:'throttle.js',
+        line:'66',
+        col:'14'
+      };
+
+    let actual = MyReporter.getErrorSource({stack:input});
+
+    assert.deepEqual(actual, expected);
+  });
+
+
+  it('AssertionError #2', function(){
+    let input = "AssertionError: [ Error: error 1\n\
+        at timeout.then (D:\\sandbox\\throttle.js:26:15),\n\
+    Error: error 2\n\
+        at timeout.then (D:\\sandbox\\thrott deepEqual [ 'job 1', 'job 2', 'job 3', 'job 4', 'job 5' ]\n\
+      at D:\\sandbox\\throttle.js:104:14";
+
+    let expected = {
+        file:'D:\\sandbox\\throttle.js',
+        line:'104',
+        col:'14'
+      };
+
+    let actual = MyReporter.getErrorSource({stack:input});
+
+    assert.deepEqual(actual, expected);
+  });
+
+
+  it('Filename contains [ and ]', function(){
+    let input = 'Error: error 1\n    at timeout.then (thr[o]ttle.js:26:15)'
+    let expected = {
+      file:'thr[o]ttle.js',
+      line:'26',
+      col:'15'
+    };
+
+    let actual = MyReporter.getErrorSource({stack:input});
+
+    assert.deepEqual(actual, expected);
+
+  });
+
+});
+
+
+
+describe('file_regex [UNIT]', function(){
+
+  it('test 1', function(){
+    let r = new RegExp("fail: \\[.*?\\] at ([\\w\\\\\\/:\\.]+):(\\d+):(\\d+)\\s*(.{0,1024})");
+
+    let input = "fail: [Throttle [UNIT] throttle success] at D:\\sandbox\\throttle.js:66:14 [ 'start 1', 'stop 5' ] deepEqual [ 'start 1', 'stop 5' ]";
+    let matchExpected = [
+      input,
+      'D:\\sandbox\\throttle.js',
+      '66',
+      '14',
+      "[ 'start 1', 'stop 5' ] deepEqual [ 'start 1', 'stop 5' ]",
+    ];
+    matchExpected.input = input;
+    matchExpected.index = 0;
+
+    let match = r.exec(input);
+    // console.log(match);
+
+    assert.deepEqual(match, matchExpected);
+
+  });
+
+});
